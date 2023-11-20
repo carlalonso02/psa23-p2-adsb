310fb55cb14c4c0127ea900ee880f391a0aca6d6
carla alonso <c.alonsoa.2020@alumnos.urjc.es>
2023-11-20 16:51:12 +0100

    eso

--- datetime_examples.py
+++ datetime_examples.py
@@ -12,6 +12,18 @@ def main():
     dt['Log datetime'] = pd.to_datetime(dt['Log Date']+ ' '+ dt['Log Time'])
     print(dt)
     print(dt.info())
+    #extraer tiempo mayores a 4 seg en x columna
+    data_4s = dt[dt['Log datetime'].dt.second > 4] #es como un filtro en la columna long time busco en que filas el tiempo es mayor que 4 sec
+    #hay .hour y mas cosas, indagar
+    print(data_4s)
+    timestamp_fist = dt['Log datetime'].iloc[0] #primer elemto columna
+    timestamp_last = dt['Log datetime'].iloc[-1] #ultimo elemento columna
+    print(f"First timestap: {timestamp_fist}")
+    print(f'Last timestap {timestamp_last}')
+
+    elapsed_time = timestamp_last - timestamp_fist
+    print(elapsed_time)
+    print(elapsed_time.total_seconds()) #
 
 if __name__ == '__main__':
     main()
