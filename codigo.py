3608af3a12732da1e68d5f66e4ab841cfad50559
calonso <c.alonsoa.2020@alumnos.urjc.es>
2023-12-13 11:58:53 +0100

    'ej_4'

--- codigo.py
+++ codigo.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #Llamdas a Dataframe
-data = pl.parse_csv()
+data = ps.parse_csv()
 
 def total_messages(data):
     '''Función para obtener el numero de mensajes totales, que sera el numero de filas del csv'''
@@ -19,18 +19,28 @@ def different_aircrafts(data):
     return print(f'Se han visto {different_ids} aviones distintos')
 
 #Número de mensajes recibidos por el sensor cada hora 
+#me da una grafica, pero no la que quiero
 def graph_messages_hour(data):
     '''Funcion para obtener el numero de mensajes recibidos por hora'''
-    
     hours_graph = data.groupby(pd.Grouper(freq='H')).count()
-    hours_graph.plot
-    plt.savefig("prueba.png",format="png")
+    hours_graph.plot(kind='bar')
+    plt.xlabel('Hora')
+    plt.ylabel('Número de mensajes')
+    plt.title('Número de mensajes recibidos por el sensor cada hora')
+    plt.xticks(rotation=45)  # Para que las horas en el eje x queden legibles
+    plt.savefig("messages_hour.png",format="png")
 
 #Número de aviones distintos vistos por el sensor cada hora (grafica)
-#lo que me dice si tengo un avion igual a otro o no es el 'Aircraft ID', tengo que agrupar los mismso valores de esa columna y sumarlos, y relacioanrlos con que hora estaba o algo asi
-#nunique() el numero de esos valores unicos
+def graph_aircrafts_message(data):
+    '''Función que representa el numero de mensajes recibido por hora de distintas aeronaves'''
+    IDs_graph = data["Aircraft ID"].groupby(data.index.hour).value_counts().unstack()
+    IDs_graph.plot(kind='bar')
+    plt.xlabel('Hora')
+    plt.ylabel('Número de aviones')
+    plt.title('Número de aviones distintos vistos por el sensor cada hora')
+    plt.legend(title='Aircraft ID', loc='upper left')
+    plt.savefig("aircraft_message.png",format="png")
+
+   
 
 #Presentación 
-total_messages(data)
-different_aircrafts(data)
-graph_messages_hour(data)
