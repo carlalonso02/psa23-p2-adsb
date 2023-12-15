b19c78d70be8553eaa7fdc52996d7f53d9cd66f1
calonso <c.alonsoa.2020@alumnos.urjc.es>
2023-12-15 16:41:21 +0100

    'las_graficas_siguen_feas'

--- codigo.py
+++ codigo.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 #Llamadas a Dataframe
 data = ps.parse_csv()
-FLIGHT = "504E2E"
+FLIGHT = "E8044F"
 def total_messages(data):
     '''Función para obtener el numero de mensajes totales, que sera el numero de filas del csv'''
     number_rows = data.shape[0]
@@ -39,10 +39,34 @@ def graph_aircrafts_message(data):
     plt.savefig("aircraft_message.png",format="png")
 
 '''calcula y muestra (en subgráficas)= subplot:
-•Mensajes totales = numero de veces que aparece esta ID en la columna ["Aircraft ID"]
 •Número de mensajes cada 5 minutos = grafica, cambbiar frqe a minutos
-•Cómo varía la altitud = variacion columna ["Altitude"] en grafica por hora/aparicion'''
+tengo que crear un dataframe filtrado, y graficarlo cada 5 mins'''
+def info_flight(data, FLIGHT):
+    ''' Función que muestra el numero de mensajes recibidos por una aeronave concreta'''
+    num = data["Aircraft ID"].value_counts()[FLIGHT]
+    return print(f'Se reciben {num} mensajes de la aeronave cuya ID es {FLIGHT}')
 
+def time_flight(data,Flight):
+    data_filter = data[data["Aircraft ID"] == FLIGHT]
+    IDs_graph = data_filter.groupby(pd.Grouper(freq = '5T')).value_counts() #5 es la frequecia 5 minutos
+    IDs_graph.plot(kind = 'bar')
+    plt.savefig("time_flight.png", format = "png")
+
+def altitude_flight(data, FLIGHT):
+    '''Función que muetsra visualemnte la variacion de la altura de vuelo de una determinada aeronave'''
+    data_filter = data[data["Aircraft ID"] == FLIGHT]
+    plt.plot(data_filter.index, data_filter[["Altitude"]], linestyle = '--', color = '#1ABC9C')
+    plt.xlabel('Hora', fontfamily="monospace")
+    plt.ylabel('Altitud', fontfamily="monospace")
+    plt.title(f'Variacion de la altitud del vuelo {FLIGHT} con el tiempo', fontfamily="monospace")
+    plt.savefig("altitude_flight.png", format = "png")
+
+#mejorar formato, aparece el numero de veces que se repite cada string, intentar que no aparezca
+def repetitive(data):
+    '''Función que obtiene los 10 aviones mas captados por el sensor'''
+    ID_times = data["Aircraft ID"].value_counts()
+    ID_repetitive = ID_times.head(10)
+    return print(f'Las 10 aernovaes uqe mas aparecen en el sensor son: {ID_repetitive}')
 #Presentación 
 print('\033[1m'+ 'Práctica 9'+ '\033[0m') #negrita
 print("\033[4;37m"+"Ejercicio 2: Análisis de datos"+"\033[0m")#subrayado
