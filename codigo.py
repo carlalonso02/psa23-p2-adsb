f2da45dac243f114246411636d8e540376af6a9c
carla alonso <c.alonsoa.2020@alumnos.urjc.es>
2023-12-18 16:38:00 +0100

    hacer_7_y_8

--- codigo.py
+++ codigo.py
@@ -2,9 +2,9 @@ import csv_short as ps
 import csv_long as pl
 import matplotlib.pyplot as plt
 import pandas as pd
-
+from geopy import distance
 #Llamadas a Dataframe
-data = ps.parse_csv()
+data = pl.parse_csv()
 FLIGHT = "E8044F"
 def total_messages(data):
     '''Función para obtener el numero de mensajes totales, que sera el numero de filas del csv'''
@@ -16,16 +16,17 @@ def different_aircrafts(data):
     different_ids = data['Aircraft ID'].nunique()
     return print(f'Se han visto {different_ids} aviones distintos')
 
-#grafica erronea, corregir!!!
+#arreglar indice eje x y quitar escala
 def graph_messages_hour(data):
     '''Funcion para obtener el numero_message de mensajes recibidos por hora'''
     hours_graph = data.groupby(pd.Grouper(freq='H')).count()
-    hours_graph.plot(kind='bar')
+    hours_graph.plot(y = 'HEX', kind='bar', color = '#AF7AC5')
     plt.autoscale()
     plt.xlabel('Hora',fontfamily="monospace")
     plt.ylabel('Número de mensajes',fontfamily="monospace")
     plt.title('Número de mensajes recibidos por el sensor cada hora',fontfamily="monospace")
     plt.savefig("messages_hour.png",format="png")
+    plt.show()
 
 def graph_aircrafts_message(data):
     '''Función que representa el numero de mensajes recibido por hora de distintas aeronaves'''
@@ -35,22 +36,26 @@ def graph_aircrafts_message(data):
     plt.xlabel('Hora', fontfamily="monospace")
     plt.ylabel('Número de aviones',fontfamily="monospace")
     plt.title('Número de aviones distintos vistos por el sensor cada hora', fontfamily="monospace")
-    plt.legend(title='Aircraft ID', loc='upper left',fontfamily="monospace") #ajustar escala bien
+    plt.legend(title='Aircraft ID', loc='upper left') #ajustar escala bien
     plt.savefig("aircraft_message.png",format="png")
+    plt.show()
 
-'''calcula y muestra (en subgráficas)= subplot:
-•Número de mensajes cada 5 minutos = grafica, cambbiar frqe a minutos
-tengo que crear un dataframe filtrado, y graficarlo cada 5 mins'''
 def info_flight(data, FLIGHT):
     ''' Función que muestra el numero de mensajes recibidos por una aeronave concreta'''
     num = data["Aircraft ID"].value_counts()[FLIGHT]
     return print(f'Se reciben {num} mensajes de la aeronave cuya ID es {FLIGHT}')
 
 def time_flight(data,Flight):
+    '''Funcion que representa el número de mensajes cada 5 minutos'''
     data_filter = data[data["Aircraft ID"] == FLIGHT]
-    IDs_graph = data_filter.groupby(pd.Grouper(freq = '5T')).value_counts() #5 es la frequecia 5 minutos
-    IDs_graph.plot(kind = 'bar')
+    IDs_graph = data_filter.groupby(pd.Grouper(freq = '5T')).count() #5 es la frequencia 5 minutos
+    IDs_graph.plot(y = 'HEX', kind = 'bar', color = '#F5B041')
+    plt.autoscale()
+    plt.xlabel('Hora',fontfamily="monospace")
+    plt.ylabel('Número de mensajes',fontfamily="monospace")
+    plt.title('Número de mensajes recibidos cada 5 minutos',fontfamily="monospace")
     plt.savefig("time_flight.png", format = "png")
+    plt.show()
 
 def altitude_flight(data, FLIGHT):
     '''Función que muetsra visualemnte la variacion de la altura de vuelo de una determinada aeronave'''
@@ -60,13 +65,40 @@ def altitude_flight(data, FLIGHT):
     plt.ylabel('Altitud', fontfamily="monospace")
     plt.title(f'Variacion de la altitud del vuelo {FLIGHT} con el tiempo', fontfamily="monospace")
     plt.savefig("altitude_flight.png", format = "png")
+    plt.show()
 
 #mejorar formato, aparece el numero de veces que se repite cada string, intentar que no aparezca
 def repetitive(data):
     '''Función que obtiene los 10 aviones mas captados por el sensor'''
     ID_times = data["Aircraft ID"].value_counts()
     ID_repetitive = ID_times.head(10)
-    return print(f'Las 10 aernovaes uqe mas aparecen en el sensor son: {ID_repetitive}')
+    return print(f'Las 10 aeronavaes que más aparecen en el sensor son:\n{ID_repetitive}')
+
+def more_repetitive(data):
+    '''Función que obtiene los 10 aviones que mas tiempo estan en el sensor'''
+    num = data.groupby(pd.Grouper(freq = '30S')).count() #agrupacion cada 30 segundos, buscar los 10 aviones que ma se repiten? usar HEX
+
+'''Muestra la distancia máxima/mínima/media de los mensajes que recibe el
+sensor. Para ello calcula la distancia (geodésica) entre las coordenadas de
+los mensajes recibidos y las coordenadas del sensor de la URJC (40.283205,
+-3.821476). Consulta https://geopy.readthedocs.io/ para calcular las dis-
+tancias (no hace falta tener en cuenta la altitud).'''
+
+def calculate_distance(data):
+    '''Función que calcula la distancia maxima y minima entre las coordenadas de los mensajes recibidos'''
+    urjc_coord = (40.283205,-3.821476) 
+    #tengo pasar los datos de las columnas latitud y longitud a un parentesis? tupla
+    coord = (data['Lat'] ,data['Lon'])
+    return print(coord)
 #Presentación 
 print('\033[1m'+ 'Práctica 9'+ '\033[0m') #negrita
 print("\033[4;37m"+"Ejercicio 2: Análisis de datos"+"\033[0m")#subrayado
+#total_messages(data)
+#different_aircrafts(data)
+#graph_messages_hour(data)
+#graph_aircrafts_message(data)
+#info_flight(data,FLIGHT)
+#time_flight(data, FLIGHT)
+#altitude_flight(data,FLIGHT)
+#repetitive(data)
+#calculate_distance(data)
