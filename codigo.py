a9a748fcb92c682769de241d11235206b37ade3f
calonso <c.alonsoa.2020@alumnos.urjc.es>
2024-01-01 11:16:12 +0100

    'casi-casi'

--- codigo.py
+++ codigo.py
@@ -17,7 +17,6 @@ def different_aircrafts(data):
     different_ids = data['Aircraft ID'].nunique()
     return print(f'Se han visto {different_ids} aviones distintos')
 
-
 def graph_messages_hour(data):
     '''Funcion para obtener el de mensajes recibidos por hora'''
     hours_graph = data.groupby(pd.Grouper(freq='H')).count()
@@ -37,7 +36,7 @@ def graph_aircrafts_message(data):
     plt.xlabel('Hora', fontfamily="monospace")
     plt.ylabel('Número de aviones',fontfamily="monospace")
     plt.title('Número de aviones distintos vistos por el sensor cada hora', fontfamily="monospace")
-    plt.legend(title='Aircraft ID', loc='upper left') #ajustar escala bien
+    plt.legend(title='Aircraft ID', loc='upper left')
     plt.savefig("aircraft_message.png",format="png")
     plt.show()
 
@@ -57,7 +56,7 @@ def time_flight(data,Flight):
     plt.title('Número de mensajes recibidos cada 5 minutos',fontfamily="monospace")
     plt.savefig("time_flight.png", format = "png")
     plt.show()
-#arregalr ejes x
+
 def altitude_flight(data, FLIGHT):
     '''Función que muetsra visualemnte la variacion de la altura de vuelo de una determinada aeronave'''
     data_filter = data[data["Aircraft ID"] == FLIGHT]
@@ -71,18 +70,16 @@ def altitude_flight(data, FLIGHT):
 def repetitive(data):
     '''Función que obtiene los 10 aviones mas captados por el sensor'''
     ID_times = data["Aircraft ID"].value_counts()
-    ID_repetitive = ID_times.head(10)
-    return print(f'Las 10 aeronavaes que más aparecen en el sensor son:\n{ID_repetitive}')
+    ID_repetitive = ID_times.head(10).index.tolist()
+    return print(f'Las 10 aeronavaes que más aparecen en el sensor en orden son:\n{ID_repetitive}')
 
-'''Obtén los 10 aviones que durante más tiempo están en el rango del sensor.
-Nótese que para identificar estos aviones, los mensajes sucesivos enviados
-por los aviones no han de haberse enviado en un intervalo mayor de 30
-segundos'''
-def more_repetitive(data):
-    '''Función que obtiene los 10 aviones que mas tiempo estan en el sensor'''
-    num = data.groupby(pd.Grouper(freq = '30S')).count() #agrupacion cada 30 segundos, buscar los 10 aviones que ma se repiten? usar HEX
- 
- #mi ordenador no lo corre, probar en laboratorio
+def  continuous_aircrafts(data):
+    '''Función que obtiene los abiones que mas tiempo de seguido han estado en el rango del sensor'''
+    time = data.groupby(['Aircraft ID', pd.Grouper(freq='30S')]).size().reset_index(name='intervalos') 
+    max_time = time.groupby('Aircraft ID')['intervalos'].max() #maximo intervalo para cada avion
+    aircrafts= max_time.sort_values(ascending=False).head(10).index.tolist() #busco los 10 aviones con intervalos mas largos
+    return print(f'Los 10 aviones que durante más tiempo han estado en el rango del sensor en orden son: \n{aircrafts}')
+
 def calculate_distance(data):
     '''Función que calcula la distancia maxima y minima entre las coordenadas de los mensajes recibidos'''
     urjc_coord = (40.283205,-3.821476) 
@@ -96,15 +93,17 @@ def calculate_distance(data):
     min_distance = min(distances) 
     media_distance = np.mean(distances)   
     return print(f'El mensaje mas lejano que recibió está a {max_distance} ,el mas cercano a {min_distance} y la media es de {media_distance} ')
+
 #Presentación 
 print('\033[1m'+ 'Práctica 9'+ '\033[0m') #negrita
 print("\033[4;37m"+"Ejercicio 2: Análisis de datos"+"\033[0m")#subrayado
-#total_messages(data)
-#different_aircrafts(data)
-#graph_messages_hour(data)
-#graph_aircrafts_message(data)
-#info_flight(data,FLIGHT)
-time_flight(data, FLIGHT)
-altitude_flight(data,FLIGHT)
-#repetitive(data)
-#calculate_distance(data)
+total_messages(data)
+different_aircrafts(data)
+graph_messages_hour(data)
+graph_aircrafts_message(data)
+info_flight(data,FLIGHT)
+time_flight(data,FLIGHT)
+altitude_flight(data, FLIGHT)
+repetitive(data)
+continuous_aircrafts(data)
+calculate_distance(data)
